f0735b4899ac187ece8f2b471c68f92a686df5c4
J2EEBeliever <J2EE.Believer@gmail.com>
2015-11-27 13:54:33 -0500

    Updates
    Updates

---
+++ SOURCE/FiveCardStudPokerWithBetting/src/ISA681Poker/AjaxLogicForm.java
@@ -0,0 +1,19 @@
+package ISA681Poker;
+
+import org.apache.struts.action.ActionForm;
+
+public class AjaxLogicForm extends ActionForm {
+	
+	private String playerContent;
+
+	public String getPlayerContent() {
+		return playerContent;
+	}
+
+	public void setPlayerContent(String playerContent) {
+		this.playerContent = playerContent;
+	}
+	
+	
+
+}
