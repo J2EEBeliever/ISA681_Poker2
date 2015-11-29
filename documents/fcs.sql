8593c0d0063e4a64785871c6a31b987531a056d7
Woltemate <bwoltemate@deloitte.com>
2015-11-29 13:23:14 -0500

    Adding documentation updates

--- documents/fcs.sql
+++ documents/fcs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2015 at 09:40 PM
+-- Generation Time: Nov 29, 2015 at 07:17 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,87 @@ SET time_zone = "+00:00";
 --
 -- Database: `fcs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+CREATE TABLE IF NOT EXISTS `game` (
+  `GameID` int(11) NOT NULL,
+  `Player1` int(11) NOT NULL,
+  `Player2` int(11) NOT NULL,
+  `GameStatus` int(11) NOT NULL,
+  `SameState` blob NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`GameID`),
+  KEY `Player1` (`Player1`),
+  KEY `Player2` (`Player2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `SessionID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserID` int(25) NOT NULL,
+  `Token` char(36) NOT NULL,
+  `Rand` bigint(11) NOT NULL,
+  `ActionCount` int(11) NOT NULL,
+  `LoginTime` datetime NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`SessionID`),
+  UNIQUE KEY `Token` (`Token`),
+  KEY `UserID` (`UserID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `session`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(20) NOT NULL,
+  `Password` char(60) NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `UserName` (`UserName`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `UserName`, `Password`, `Timestamp`) VALUES
+(1, 'b1wolt', '$2a$12$BW5zGtaLGIbp.a13JusvZ.zQGqbBy8cW..02nCQT6wggregArnOhe', '2015-11-28 11:15:38');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`Player1`) REFERENCES `users` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`Player2`) REFERENCES `users` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `session`
+--
+ALTER TABLE `session`
+  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
